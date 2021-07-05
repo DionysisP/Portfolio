ef30a7032dad95b274e76b89f08f3304d4568b7f
DionysisP <dion.patsiouras@gmail.com>
2021-07-05 23:27:11 +0300

    show data with json

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,22 +4,24 @@ import Project from './portfolioComponents/Project';
 import cvc from '../images/cvc.png';
 import cinema from '../images/cinema.png';
 import lyrics from '../images/lyrics.jpg';
+import data from './projects.json';
 
-export default function Portfolio(props){
-    return(
-        <div className="wrapper2" style={{'paddingTop' : '64px'}}>
+export default function Portfolio(props) {
+
+
+
+    return (
+
+        <div className="wrapper2" style={{ 'paddingTop': '64px' }}>
             <h1>{props.title}</h1>
-            <div className={classes.projects}>             
-                <Project id='1' projectTitle='Covid Vaccine Company' image={cvc} projectDesc='Quisque molestie, est laoreet congue consectetur, turpis ligula ornare risus, ut auctor ex felis at nulla.'/>
-                <Project id='2' projectTitle='Melbourne Theater' image={cinema} projectDesc='Turpis ligula ornare risus, ut auctor ex felis at nulla.'/>
+            <div className={classes.projects}>
+                {data.map(proj => (
+                    <Project id={proj.id} projectTitle={proj.title} image={proj.image} projectDesc={proj.description} />
+                ))}
 
             </div>
-            <div className={classes.projects}>             
-                <Project id='1' projectTitle='Covid Vaccine Company' image={cvc} projectDesc='Quisque molestie, est laoreet congue consectetur, turpis ligula ornare risus, ut auctor ex felis at nulla.'/>
-                <Project id='2' projectTitle='Melbourne Theater' image={cinema} projectDesc='Turpis ligula ornare risus, ut auctor ex felis at nulla.'/>
 
-            </div>
-            
+
         </div>
-    );  
+    );
 }
