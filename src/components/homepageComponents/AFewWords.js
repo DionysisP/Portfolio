87d22997e588d91c6b0a9d0f86dd335f00a82177
DionysisP <dion.patsiouras@gmail.com>
2021-07-05 20:14:46 +0300

    Add dynamic age

--- src/components/homepageComponents/AFewWords.js
+++ src/components/homepageComponents/AFewWords.js
@@ -3,10 +3,12 @@ import classes from '../style/AFewWords.module.css';
 
 export default function AFewWords(props){
 
+    const currentDate = new Date().getFullYear();
+
     return(
         <div className="wrapper">
             <h1>{props.title}</h1>
-            <p className={classes.paragraph}>I am 26 years old. My hometown is Larisa, Greece. I studied Web - Desing & Video Games Development 
+            <p className={classes.paragraph}>I am {currentDate - 1995} years old. My hometown is Larisa, Greece. I studied Web - Desing & Video Games Development 
             in Thessaloniki for 2 years. I am very passionate and I study and work very hard every day.
             I like learn new things and become better day by day.</p> 
             <p style={{'paddingTop' : '66px'}} className={classes.paragraph}>Find me on LinkedIn : <span><a style={{"color" :"#f81783"}} href="https://www.linkedin.com/in/dionysis-patsiouras-a10415213/" target="_blank" rel="noreferrer">Dionysis Patsiouras</a></span></p>
