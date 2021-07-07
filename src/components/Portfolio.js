8ae3896bc7707faf42cd0ebf40547fd6db3f8579
DionysisP <dion.patsiouras@gmail.com>
2021-07-07 19:41:11 +0300

    portfolio showcase

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 
 import ShowProject from './portfolioComponents/ShowProject';
-
+import style from './style/Portfolio.module.css';
 import cvc from '../images/cvc.png';
 import cinema from '../images/cinema.png';
 export default function Portfolio(props) {
@@ -10,10 +10,10 @@ export default function Portfolio(props) {
         <div className="wrapper2" style={{ 'paddingTop': '64px' }}>
 
             <h1>{props.title}</h1>
-            
-            <ShowProject title='CVC' description='elajdsadas' image={cvc}/>
-            <ShowProject title='Cinema' description='sdasda' image={cinema}/>
-
+            <div className={style.allProjects}>
+                <ShowProject title='CVC' description='elajdsadas' image={cvc} />
+                <ShowProject title='Cinema' description='sdasda' image={cinema} />
+            </div>
         </div>
     );
 }
--- src/components/portfolioComponents/ShowProject.js
+++ src/components/portfolioComponents/ShowProject.js
@@ -1,12 +1,13 @@
-import classes from '../style/Portfolio.module.css';
 
-export default function ShowProject(props){
-    return(
-        <div className={classes.individualProj}>
-        <img src={props.image} alt='Project' />
-        <h4 className={classes.projectTitle}>{props.title}</h4>
-        <p className={classes.projectDesc}>{props.description}</p>
-    </div>
+import style from '../style/ShowProject.module.css';
+
+export default function ShowProject(props) {
+    return (
+        <div className={style.singleProject}>
+            <img src={props.image} alt='Project' />
+            <h4 className={style.projectTitle}>{props.title}</h4>
+            <p className={style.projectDesc}>{props.description}</p>
+        </div>
     )
 
 }
