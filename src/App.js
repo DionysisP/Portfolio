0b1bdee93f9d14756460bd14ce45743e35963492
DionysisP <dion.patsiouras@gmail.com>
2021-07-07 21:21:41 +0300

    portfolio changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {Route, Switch} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import Navigation from "./components/Navigation";
 import Homepage from './components/Homepage.js';
 import ScrollToTop from './components/ScrollToTop';
@@ -27,10 +27,12 @@ function App() {
             <Homepage />
       
             <div className="up">
+              <Link to={'/'} >
               <svg width="58" height="58" viewBox="0 0 58 58" fill="none" xmlns="http://www.w3.org/2000/svg">
               <circle cx="29" cy="29" r="29" fill="#F81783"/>
               <path d="M30.0607 22.9393C29.4749 22.3536 28.5251 22.3536 27.9393 22.9393L18.3934 32.4853C17.8076 33.0711 17.8076 34.0208 18.3934 34.6066C18.9792 35.1924 19.9289 35.1924 20.5147 34.6066L29 26.1213L37.4853 34.6066C38.0711 35.1924 39.0208 35.1924 39.6066 34.6066C40.1924 34.0208 40.1924 33.0711 39.6066 32.4853L30.0607 22.9393ZM30.5 26L30.5 24L27.5 24L27.5 26L30.5 26Z" fill="white"/>
               </svg>
+              </Link>
             </div>
 
             <Footer title="Contact"/>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,8 +11,10 @@ export default function Portfolio(props) {
 
             <h1>{props.title}</h1>
             <div className={style.allProjects}>
-                <ShowProject title='CVC' description='elajdsadas' image={cvc} />
-                <ShowProject title='Cinema' description='sdasda' image={cinema} />
+                <ShowProject title='CVC' description='Nullam ut dui vulputate, semper lacus et, feugiat arcu. Morbi justo lacus, mollis at augue in, eleifend semper enim. Mauris ipsum tortor, convallis.' image={cvc} />
+                <ShowProject title='Cinema' description='Nam interdum tempus lectus id blandit. Cras posuere convallis orci, nec tempus augue maximus sit amet. Pellentesque sit amet aliquam magna.' image={cinema} />
+                <ShowProject title='CVC' description='Praesent faucibus purus vitae placerat lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.' image={cvc} />
+        
             </div>
         </div>
     );
