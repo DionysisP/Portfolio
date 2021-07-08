cd702d46baea31ab3e919f8b09e8828b60411659
DionysisP <dion.patsiouras@gmail.com>
2021-07-08 13:55:18 +0300

    add alt property on images

--- src/components/portfolioComponents/ShowProject.js
+++ src/components/portfolioComponents/ShowProject.js
@@ -7,13 +7,13 @@ export default function ShowProject(props) {
     return (
         <div className={style.singleProject}>
 
-            <img className={style.vectorDesktop} src={desktop} />
+            <img className={style.vectorDesktop} src={desktop} alt='Desktop Vector'/>
             
-            <img className={style.projImg} src={props.image} />
+            <img className={style.projImg} src={props.image} alt='Project' />
             
-            <img className={style.vectorMobile} src={mobile} />
+            <img className={style.vectorMobile} src={mobile} alt='Mobile Vector' />
             
-            <img className={style.projImg2} src={props.image2} />
+            <img className={style.projImg2} src={props.image2} alt='Project' />
             
             <h4 className={style.projectTitle}>{props.title}</h4>
             <p className={style.projectDesc}>{props.description}</p>
