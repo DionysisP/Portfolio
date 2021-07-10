5f8eb22c5178a3e63ee93846a1e78e8990ef84f2
DionysisP <dion.patsiouras@gmail.com>
2021-07-10 17:25:54 +0300

    delete photos

--- src/components/homepageComponents/SomeOfMyWork.js
+++ src/components/homepageComponents/SomeOfMyWork.js
@@ -1,18 +1,15 @@
 import classes from '../style/SomeOfMyWork.module.css';
+import { Link } from 'react-router-dom';
 
-import {Link} from 'react-router-dom';
+export default function SomeOfMyWork(props) {
 
-export default function SomeOfMyWork(props){
-
-    return(
+    return (
         <div className="wrapper">
             <h1>{props.title}</h1>
             <div className={classes.display}>
-                <img className={classes.firstPhoto} alt="Project"/>
-                <img className={classes.secondPhoto} alt="Project"/>
             </div>
             <Link to='/portfolio'>
-            <button className={classes.portfolioButton}>Portfolio</button>
+                <button className={classes.portfolioButton}>Portfolio</button>
             </Link>
         </div>
     );
