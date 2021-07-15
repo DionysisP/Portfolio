e7edbbed38e1d34703175a79d6c8f4fc3fe3c743
DionysisP <dion.patsiouras@gmail.com>
2021-07-15 19:02:09 +0300

    minor changes and optimization

--- src/components/portfolioComponents/Insert.js
+++ src/components/portfolioComponents/Insert.js
@@ -3,7 +3,7 @@ export default function Insert(props){
     return(
         <section>
             <p className={style.year}>{props.year}</p>
-            <div className={style.contain} style={{'marginTop' : '0px'}}>
+            <div className={style.contain} style={{'marginTop' : '-1px'}}>
             <p className={style.edu}>{props.title}</p>
             <p className={style.description}>{props.description}</p>
             </div>
