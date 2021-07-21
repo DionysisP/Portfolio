7da52b2a6b4d4b74a5311b73fdfb6890e4ec85d6
DionysisP <dion.patsiouras@gmail.com>
2021-07-21 13:27:26 +0300

    improve code

---
+++ src/components/ProjectPage.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import classes from '../style/Project.module.css'
+import style2 from '../style/Modal.module.css'
+
+import Modal from './portfolioComponents/Modal';
+import ViewCodeButton from './portfolioComponents/ViewCodeButton'
+
+import close from '../../src/images/icons/close.svg'
+import arrow from '../../src/images/icons/arrow.svg'
+
+
+export default function ProjectPage(props) {
+
+
+    return (
+
+        <div className="wrapper2" style={{ 'paddingTop': '64px' }}>
+
+            <h1 className={classes.title}>{props.title}</h1>
+            <div className={classes.back}><img src={arrow} alt='arrow' /><Link to={'/Portfolio'}> Back to Projects</Link></div>
+            <div className={classes.mainContainer}>
+
+                <div className={classes.container1}>
+                    <a href={props.url} target="_blank" rel="noreferrer">{props.url}</a>
+                    <p className={classes.desc}>{props.desc}</p>
+                    <p className={classes.tech}>{'Technologies : ' + props.technologies}</p>
+                    <ViewCodeButton url={props.git} />
+                </div>
+
+                <div className={classes.container2} >
+                    <img className={classes.websiteImg} src={require('../images/' + props.websiteImg + '.jpg').default} alt='website' />
+                </div>
+            </div>
+        </div>
+
+
+    );
+}
