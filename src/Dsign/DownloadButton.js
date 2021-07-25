0f22a08a5be4a27160060fbd420f953d08f1635c
DionysisP <dion.patsiouras@gmail.com>
2021-07-26 02:03:24 +0300

    fix file name on download

--- src/Dsign/DownloadButton.js
+++ src/Dsign/DownloadButton.js
@@ -2,10 +2,8 @@ import download from './download-white.svg'
 import style from '../Dsign/Dsign.module.css'
 
 export default function DownloadButton(props) {
-
-
     return (
-        <a href={require('./icons/' + props.folder + '/' + props.file + props.filetype).default} download>
+        <a href={require('./icons/' + props.folder + '/' + props.file + props.filetype).default} download={props.file}>
             <button className={style.buttonTag}>{props.text}<img className={style.buttonImg} src={download} alt='download' /></button>
         </a >
     );
